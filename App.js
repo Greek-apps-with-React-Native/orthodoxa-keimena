8665b6b5188ec12691d331c5b7d35855c940b0e7
Fotis Tsakiris <footios76@gmail.com>
2019-09-09 10:31:22 +0300

    fonts added + use of loadAsync and Apploading

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const fetchFonts = () => {
+	Font.loadAsync({
+		'GFSNeohellenic-Bold': require('./assets/fonts/GFSNeohellenic-Bold.ttf'),
+		'GFSNeohellenic-BoldItalic': require('./assets/fonts/GFSNeohellenic-BoldItalic.ttf'),
+		'GFSNeohellenic-Regular': require('./assets/fonts/GFSNeohellenic-Regular.ttf'),
+		'GFSNeohellenic-RegularItalic': require('./assets/fonts/GFSNeohellenic-RegularItalic.ttf')
+	});
+};
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	const [ fontLoaded, setFontLoaded ] = useState(false);
+
+	if (!fontLoaded) {
+		return <AppLoading startAsync={fetchFonts} onFinish={setFontLoaded(true)} />;
+	}
+
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.js to start working on your app!</Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
---
+++ README.md
@@ -0,0 +1,12 @@
+This is my first personal project with React Native and expo!
+Don't quit!
+Success! 
+Happy coding :)
+
+### Packages installed:
+#### expo-font
+
+
+### Branches explained: 
+
+

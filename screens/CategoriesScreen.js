209615217014a1c0c9e1053552852b25575b170f
Fotis Tsakiris <footios76@gmail.com>
2019-09-09 10:07:45 +0300

    first commit & screens added

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,23 @@
+// Here you can select the thema/category of the books,
+// e.g. Biblical, Dogmatic, History etc
+
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const CategoriesScreen = (props) => {
+	return (
+		<View style={styles.screen}>
+			<Text>The Categories Screen</Text>{' '}
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
+});
+
+export default CategoriesScreen;
---
+++ screens/FiltersScreen.js
@@ -0,0 +1,1 @@
+// Books of saints, scholars, etc

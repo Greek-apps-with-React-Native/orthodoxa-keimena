1105da59930c671d753efd4bdf041e20ad377f30
Fotis Tsakiris <footios76@gmail.com>
2019-09-13 13:04:43 +0300

    change color + Icon in defaultNavigationOptions

--- navigation/BooksNavigator.js
+++ navigation/BooksNavigator.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
-import { FontAwesome } from '@expo/vector-icons';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { FontAwesome, FontAwesome5, MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
 
 
 import CategoriesScreen from '../screens/CategoriesScreen';
@@ -38,18 +37,46 @@ const BooksNavigator = createStackNavigator({
 const FavBottomTabNavigator = createBottomTabNavigator({
 	Books: {
 		screen:BooksNavigator,
-		navigationOptions: {
-			tabBarIcon: (tabInfo) => <FontAwesome name='book' size={25} color={tabInfo.tintColor} />
-		}
+		// navigationOptions: {
+		// 	tabBarIcon: (tabInfo) => <FontAwesome name='book' size={25} color={tabInfo.tintColor} />
+		// }
 	}, 
 	Favorites: {
 		screen: FavoritesScreen, 
-		navigationOptions: {
-			tabBarIcon: tabInfo => <MaterialCommunityIcons name='bolnisi-cross' size={25} color={tabInfo.tintColor} />
-		}
+		// navigationOptions: {
+		// 	tabBarIcon: tabInfo => <MaterialCommunityIcons name='bolnisi-cross' size={25} color={tabInfo.tintColor} />
+		// }
 	},	
 }, {
-	activeTintColor: Colors.moccasin
+	defaultNavigationOptions: ({ navigation }) => ({
+		// Ionicons = ios-book
+		// MaterialCommunityIcons = book
+		// FontAwesome = cross
+		// FontAwesome5 = bolnisi-cross
+		tabBarIcon: ({ focused, tintColor }) => {
+			const { routeName } = navigation.state;
+			let iconName='';
+			if (routeName === 'Books') {
+				iconName = focused ? 'ios-book' : 'book';		
+			} else if (routeName === 'Favorites') {
+				iconName = focused ? 'bolnisi-cross' : 'cross';
+			}
+			if (routeName === 'Books' && focused) {
+				return <Ionicons name={iconName} size={25} color={tintColor} />;
+			} else if (routeName === 'Favorites' && focused) {
+				return <MaterialCommunityIcons name={iconName} size={25} color={tintColor} />;
+			}
+			if (routeName === 'Books' && !focused) {
+				return <FontAwesome name={iconName} size={25} color={tintColor} />;
+			} else if (routeName === 'Favorites' && !focused) {
+				return <FontAwesome5 name={iconName} size={25} color={tintColor} />;
+			}
+		}
+	}),
+
+	tabBarOptions: {
+		activeTintColor: Colors.accentColor
+	}
 })
 
 export default createAppContainer(FavBottomTabNavigator);

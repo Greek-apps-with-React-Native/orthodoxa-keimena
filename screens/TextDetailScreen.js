26557ead47d6503f6061c24af16f2341571d9a7f
Fotis Tsakiris <footios76@gmail.com>
2019-09-13 10:08:04 +0300

    PinchZoomView not good for text...

--- screens/TextDetailScreen.js
+++ screens/TextDetailScreen.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import { StyleSheet, WebView } from 'react-native';
+import PinchZoomView from 'react-native-pinch-zoom-view';
+
 import { MODULES } from '../data/modules';
 
 const TextDetailScreen = (props) => {
 	const moduleId = props.navigation.getParam('moduleId');
 	const displayedModule = MODULES.find((module) => module.id === moduleId);
-		 
-	return <WebView style={styles.webview}  source={{html: displayedModule.moduleText}} />
+
+	return (
+		<PinchZoomView>
+			<WebView style={styles.webview} source={{ html: displayedModule.moduleText }} />
+		</PinchZoomView>
+	);
 };
 
-TextDetailScreen.navigationOptions = navData => {
+TextDetailScreen.navigationOptions = (navData) => {
 	const moduleId = navData.navigation.getParam('moduleId');
 	const displayedModule = MODULES.find((module) => module.id === moduleId);
 
 	return {
 		headerTitle: displayedModule.heading
-	}
-}
+	};
+};
 
 const styles = StyleSheet.create({
 	webview: {
 		alignContent: 'flex-start',
-		margin: 15, 
+		margin: 15
 	}
-})
+});
 
 export default TextDetailScreen;

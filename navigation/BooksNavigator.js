162abf59654ee9464d048dccd8756578e245e4d8
Fotis Tsakiris <footios76@gmail.com>
2019-09-13 12:20:39 +0300

    createBottomTabNavigator + icons for the tabs

--- navigation/BooksNavigator.js
+++ navigation/BooksNavigator.js
@@ -1,10 +1,17 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { FontAwesome } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryBooksScreen from '../screens/CategoryBooksScreen';
 import CategoryModulesScreen from '../screens/CategoryModulesScreen';
 import TextDetailScreen from '../screens/TextDetailScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
+
 
 import Colors from '../constants/Colors';
 
@@ -28,4 +35,22 @@ const BooksNavigator = createStackNavigator({
         }
 });
 
-export default createAppContainer(BooksNavigator);
+const FavBottomTabNavigator = createBottomTabNavigator({
+	Books: {
+		screen:BooksNavigator,
+		navigationOptions: {
+			tabBarIcon: (tabInfo) => <FontAwesome name='book' size={25} color={tabInfo.tintColor} />
+		}
+	}, 
+	Favorites: {
+		screen: FavoritesScreen, 
+		navigationOptions: {
+			tabBarIcon: tabInfo => <MaterialCommunityIcons name='bolnisi-cross' size={25} color={tabInfo.tintColor} />
+		}
+	},	
+}, {
+	activeTintColor: Colors.moccasin
+})
+
+export default createAppContainer(FavBottomTabNavigator);
+ 

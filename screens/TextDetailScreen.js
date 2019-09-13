e1d2ff5225c7ae0ebb055868e9aab18ffc4dbcca
Fotis Tsakiris <footios76@gmail.com>
2019-09-13 10:23:20 +0300

    Set <h1 style='font-size: 300%;'> in WebView

--- screens/TextDetailScreen.js
+++ screens/TextDetailScreen.js
@@ -1,34 +1,30 @@
 import React from 'react';
 import { StyleSheet, WebView } from 'react-native';
-import PinchZoomView from 'react-native-pinch-zoom-view';
+
 
 import { MODULES } from '../data/modules';
 
 const TextDetailScreen = (props) => {
 	const moduleId = props.navigation.getParam('moduleId');
 	const displayedModule = MODULES.find((module) => module.id === moduleId);
-
-	return (
-		<PinchZoomView>
-			<WebView style={styles.webview} source={{ html: displayedModule.moduleText }} />
-		</PinchZoomView>
-	);
+		 
+	return  <WebView style={styles.webview}  source={{html: displayedModule.moduleText}} />
 };
 
-TextDetailScreen.navigationOptions = (navData) => {
+TextDetailScreen.navigationOptions = navData => {
 	const moduleId = navData.navigation.getParam('moduleId');
 	const displayedModule = MODULES.find((module) => module.id === moduleId);
 
 	return {
 		headerTitle: displayedModule.heading
-	};
-};
+	}
+}
 
 const styles = StyleSheet.create({
-	webview: {
+	webview: { // type: View...
 		alignContent: 'flex-start',
-		margin: 15
+		margin: 15, 
 	}
-});
+})
 
 export default TextDetailScreen;
